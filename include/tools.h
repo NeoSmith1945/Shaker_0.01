b5553272f12f39cee4a6ad971660f87b1b3b01bc
Neo Green67 <alex@2a-gruener.de>
2022-03-30 15:01:21 +0200

    clean main

--- include/tools.h
+++ include/tools.h
@@ -1,5 +1,10 @@
 #include <driver/adc.h>
 #include <analogWriter.h>
+#include "WebServer.h"
+#include "WebSocketsServer.h"
+#include <ArduinoJson.h>                             
+
+#include "SPIFFS.h"
 
 // Include Adafruit PCA9685 Servo Library
 #include "Servo_PCA9685.h"
@@ -38,3 +43,7 @@ void current_monitor_task(void *param);
 void shakeTaskPerTimer( void *param ) ;
 void shakeTask( void *param ) ;
 void VoltageMonTask( void *param );
+void update_ui();
+void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length);
+void sendJson(String l_type, String l_value);
+void loadConfiguration(const char *filename, Config &config);
